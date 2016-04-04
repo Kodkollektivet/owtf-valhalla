86a6980e3267009523b2692212a0fa857172c075
rasjostrom <ras.sjostrom@hotmail.com>
2016-04-03 22:32:11 -0400

    Temporary api version check workaround
    The docker client and docker server needs to be running the same API
    version. This checks the server version and sets the client to use an
    older version of the API if necessary.

--- owtfvalhalla/owtfcontainer/dclient.py
+++ owtfvalhalla/owtfcontainer/dclient.py
@@ -8,11 +8,18 @@ the docker client.
 Ex:
 from dclient import dclient as cli
 """
+import subprocess
 from sys import platform
 
 if platform == "linux" or platform == "linux2":
     from docker import Client
-    cli = Client(base_url='unix://var/run/docker.sock')
+
+    # workaround to make sure client and server uses the same api version
+    # is there a better way to handle this? !help-wanted
+    docker_version_bytes = subprocess.check_output(['docker','version'])
+    api_version = docker_version_bytes.decode('utf-8').splitlines()[1][20:]
+
+    cli = Client(base_url='unix://var/run/docker.sock', version=api_version)
 else:
     import docker
     cli = docker.from_env(assert_hostname=False)

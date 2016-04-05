0af332cfd43246122fcde57265e6b17d5dba83f7
jherrlin <jherrlin@gmail.com>
2016-04-05 21:22:36 +0200

    test for owtfcontainer facade added, more tests is needed

---
+++ test/test_handler.py
@@ -0,0 +1,18 @@
+import unittest
+from owtfvalhalla.owtfcontainer import *
+
+
+class OwtfContainerTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_verifytypes_true(self):
+        ocs = get_owtf_c()
+        self.assertTrue(isinstance(ocs[0], bool))
+        self.assertTrue(isinstance(ocs[1], list))
+
+

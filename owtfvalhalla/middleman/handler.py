f9fd089ce09c80148e88de0ac573dfb937545c7b
jherrlin <jherrlin@gmail.com>
2016-04-10 23:10:06 +0200

    Middleman handler updated.
    The passing of command works. The while loop needs to be looked over.

--- owtfvalhalla/middleman/handler.py
+++ owtfvalhalla/middleman/handler.py
@@ -6,20 +6,20 @@ import time
 def send_for_execution(ipaddress, cmd):
     """Sends the command to the container and asks for result.
     """
+    cmd = json.dumps(cmd)
+    requests.post('http://'+ipaddress+':5000/run', data=cmd)
 
-    remote_host = 'http://'+ipaddress+':5000/run'
-    requests.post(remote_host, data=cmd)
-
-    result_request = requests.get('http://'+ipaddress+'/result')
+    result_request = requests.get('http://'+ipaddress+':5000/result')
 
     while result_request.json()['status'] != 0:
-        result_request = requests.get('http://'+ipaddress+'/result')
+        result_request = requests.get('http://'+ipaddress+':5000/result')
+        print(result_request.text)
         time.sleep(2)
 
     return result_request.json()
 
 
 if __name__ == '__main__':
-    a = send_for_execution('172.17.0.2', {'command': 'ping -c 2', 'target': 'idg.se'})
+    a = send_for_execution('172.17.0.3', json.dumps({'command': 'ping -c 2 idg.se'}))
 
 

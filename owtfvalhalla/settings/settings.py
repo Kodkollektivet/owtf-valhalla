3bb420e21a45cf2b6699ba21bfcc5712221cd67d
jherrlin <jherrlin@gmail.com>
2016-04-10 23:46:43 +0200

    API template changed to fit OWTF Valhalla

--- owtfvalhalla/settings/settings.py
+++ owtfvalhalla/settings/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "web/templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

796c0ed164669bd39dfca23590b0ce0456d37473
jherrlin <jherrlin@gmail.com>
2016-04-24 19:42:43 +0200

    Flask API updated to newest version.
    Gitignore updated.

--- containers/testcontainer/app.py
+++ containers/testcontainer/app.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-import os
-from subprocess import Popen, PIPE
+import json
+import subprocess
 from flask import Flask, request, jsonify, url_for
-
-from cfgutils import read_config, dump_config_json, valid_cfg
 
 app = Flask(__name__)
 
@@ -15,20 +13,41 @@ process = None
 @app.route("/")
 def index():
     """Returns information about the command and possible routes"""
-    if not valid_cfg(dump_config_json()):
-        cfg_error = "The specified configuration file is invalid"
-        return jsonify({"description": cfg_error})
-    
-    return jsonify({"description": read_config('description')})
+    with open('config.json') as configfile:
+        configdata = json.load(configfile)
+
+    return jsonify({'name': configdata['title'],
+                    'version': configdata['version'],
+                    'description': configdata['description'],
+                    'routes': [
+                        {
+                            'path': '/run',
+                            'method': ['POST'],
+                            'description': 'Send POST to execute command'
+                        },
+                        {
+                            'path': '/result',
+                            'method': ['GET'],
+                            'description': 'Get results, if result: false it still runs'
+                        },
+                        {
+                            'path': '/config',
+                            'method': ['GET'],
+                            'description': 'Get available commands'
+                        }]
+                    })
 
 
-@app.route("/run")
+@app.route("/run", methods=['POST','HEAD'])
 def run():
     """Starts the subproccess"""
     global process
 
-    script = read_config('command')
-    process = Popen([script], stdout=PIPE)
+    req = request.get_json(force=True)
+
+    execute = req.get('command')
+
+    process = subprocess.Popen(execute, shell=True, stdout=subprocess.PIPE)
     return jsonify({"message": "Subprocess started"})
 
 
@@ -38,14 +57,21 @@ def result():
     global process
 
     if not process:
-        return jsonify({"response": "No running proccess"})
+        return jsonify({"status": 1, "response": "No running proccess"})
     elif not process.poll() == 0:
-        return jsonify({"response": "Process still running"})
+        return jsonify({"status": 2, "response": "Process still running"})
 
     res = process.communicate()
     process.kill()
     process = None
-    return jsonify({"response": str(res[0])})
+    return jsonify({"status": 0, "response": str(res[0])})
+
+
+@app.route("/config", methods=['GET'])
+def config():
+    """Used is container is external"""
+    with open('config.json') as configfile:
+        return jsonify(json.load(configfile))
 
 
 if __name__ == "__main__":

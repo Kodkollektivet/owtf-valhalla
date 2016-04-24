81c89eae7b4382578ff17aa3c66affc1963b5e9f
jherrlin <jherrlin@gmail.com>
2016-04-24 20:13:24 +0200

    Cors headers for Core API.
    Allow external call to the Core API.
    django-cors-headers
    https://github.com/ottoyiu/django-cors-headers/

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'web',
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
+
+# CORE headers
+CORS_ORIGIN_ALLOW_ALL = True

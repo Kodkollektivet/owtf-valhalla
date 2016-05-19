81f5c183311e8f42a71a1f38bd151339b0b33a88
lazy <sm222gb@student.lnu.se>
2016-05-20 01:00:19 +0200

    exceptions.py now holds all the exceptions. It was the only way to satisfy all the scripts and the import path issue.

--- core/dockerutils/exceptions.py
+++ core/dockerutils/exceptions.py
@@ -2,7 +2,6 @@ class DockerContainerException(Exception):
     def __init__(self, exception_message):
         super(DockerContainerException, self).__init__(exception_message)
 
-
 class DockerImageException(Exception):
     def __init__(self, exception_message):
         super(DockerImageException, self).__init__(exception_message)

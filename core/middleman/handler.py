c1f66f6b3e6d119fcb9c96430504a01e7ca96383
jherrlin <jherrlin@gmail.com>
2016-06-03 21:41:59 +0200

    Middleman handler fixes.
    Updated the middleman handler for sending commands to the containers.

--- core/middleman/handler.py
+++ core/middleman/handler.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import time
+from datetime import datetime
 
 
-def send_for_execution(ipaddress, port, cmd):
+def send_for_execution(image, cmd):
     """Sends the command to the container and asks for result.
     """
+    cmd_org = cmd
     cmd = json.dumps(cmd)
 
-    address = 'http://' + ipaddress + ':' + str(port) + '/'
+    start_time = datetime.now().strftime('%d-%m-%y %H:%M:%S')
+    address = 'http://' + image.ip_address + ':' + str(image.port) + '/'
 
     requests.post(address + 'run', data=cmd)
 
@@ -19,5 +22,11 @@ def send_for_execution(ipaddress, port, cmd):
         print(result_request.text)
         time.sleep(2)
 
-    return result_request.json()
+    return {
+        'start_time': start_time,
+        'stop_time': datetime.now().strftime('%d-%m-%y %H:%M:%S'),
+        'result': result_request.json()['response'],
+        'command': cmd_org,
+        'image': image.image
+    }
 

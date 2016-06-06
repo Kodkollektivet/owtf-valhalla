0c700ffdadabf770e75441403ff14228c4f24e84
Sebastian <sebastian.lindstrom@gmail.com>
2016-06-06 15:12:40 +0200

    Grouped the commands together under the same codes

--- test/test_api.py
+++ test/test_api.py
@@ -70,8 +70,4 @@ class TestApiCommandsObject(APITestCase):
         response = self.client.get('/containers/commands/')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.data[0]['code'], '666')
-<<<<<<< 4f86c2149c2c77663d3460cdf890b9be5dc88f1b
         self.assertEquals(response.data[0]['commands'][0]['image'], 'owtfvalhallatestcontainer:0.1')
-=======
-        self.assertEquals(response.data[0]['image'], 'owtfvalhallatestcontainer:0.1')
->>>>>>> Restructured command list API response to be just a list of command objects instead of an object itself where every command is a field with the code as a name. The reason for this being that the React component in the client should be able to iterate it as an array of objects

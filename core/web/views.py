2fdef8ce6ff225c0d75840d0857fe8966fa714db
Sebastian <sebastian.lindstrom@gmail.com>
2016-06-29 12:49:19 +0200

    Added serializers for new command list structure

--- core/web/views.py
+++ core/web/views.py
@@ -156,6 +156,7 @@ class Commands(APIView):
         serializer = serializers.CodeSerializer(commands, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 class Execute(APIView):
     """Get a command and the pass it on to the associated container"""
 

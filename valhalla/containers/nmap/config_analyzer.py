9292c101ab9795356ef880dfad96a86e5512f050
oerlex <alex.sp@posteo.de>
2016-07-15 12:16:23 +0200

    Added the nikto and httprint container
    and made changes to the config_analyzer file

--- valhalla/containers/nmap/config_analyzer.py
+++ valhalla/containers/nmap/config_analyzer.py
@@ -5,31 +5,30 @@ x=[]
 p = open('config.json')
 u = p.read()
 config = json.loads(u)
-file = open('config_analyzer_log.txt', 'w+')
 
 #Function that checks if there is duplicated codes (Ex. OTG-INFO-004) in the configuration file
 def checkDuplicateCodes():
-    file.write("Checking duplicate codes...\n")
     for i in config['commands']:
         x.append(i['code'])
 
     if len(x) != len(set(x)):
         print('There are duplicate codes')
-        file.write('There are duplicate codes in the config file\n')
+
     else:
         print('There are no duplicate codes')
-        file.write('There are duplicate codes in the config file\n')
 
 #Function that checks if there is commands with keys that have no values
 #in the configuration file
 def checkEmptyValues():
-    file.write("Checking empty key-values...\n")
+    flag = True
     for i in config['commands']:
         for key, value in i.iteritems():
-            print(key, value)
-            if not value:
-                file.write("Command: "+str(i['code'])+" The Key "+str(key)+" has no value\n")
-                print("no value in the key")
+            if value=='?':
+                print("The The Key "+str(key)+" from the command "+str(i['command'])+" has no value")
+                flag = False
+    if flag:
+        print("There are no empty key-values")
+
 
 
 checkEmptyValues()
